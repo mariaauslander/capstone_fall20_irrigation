8eef911393c6cb27fd114ca71e8c5485569138be
tom <tomgoter@gmail.com>
2020-10-01 16:03:32 -0400

    pushing scripts for cloud

---
+++ supervised_classification.py
@@ -0,0 +1,171 @@
+import pandas as pd
+import tensorflow as tf
+from glob import glob
+import os
+from matplotlib import pyplot as plt
+import numpy as np
+from tqdm import tqdm
+import seaborn as sns
+#from matplotlib.cm import get_cmap
+import csv
+import json
+import time
+from tensorflow.keras.applications import ResNet50, ResNet101V2, Xception, InceptionV3
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from utils import *
+import argparse
+
+print(f'Using TensorFlow Version: {tf.__version__}')
+sns.set()
+
+# Set Paths
+BASE_PATH = './BigEarthData'
+OUTPUT_PATH = os.path.join(BASE_PATH, 'models')
+TFR_PATH = os.path.join(BASE_PATH, 'tfrecords')
+
+def get_training_dataset(training_filenames, batch_size):
+  return get_batched_dataset(training_filenames, batch_size)
+
+def get_validation_dataset(validation_filenames, batch_size):
+  return get_batched_dataset(validation_filenames, batch_size)
+
+METRICS = [
+      tf.keras.metrics.TruePositives(name='tp'),
+      tf.keras.metrics.FalsePositives(name='fp'),
+      tf.keras.metrics.TrueNegatives(name='tn'),
+      tf.keras.metrics.FalseNegatives(name='fn'),
+      tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+      tf.keras.metrics.Precision(name='precision'),
+      tf.keras.metrics.Recall(name='recall'),
+      tf.keras.metrics.AUC(name='auc'),
+]
+
+def build_model(imported_model, use_pretrain, metrics = METRICS, output_bias=None):
+  if output_bias is not None:
+    output_bias = tf.keras.initializers.Constant(output_bias)
+  if use_pretrain:
+    model = imported_model(include_top=False, weights='imagenet', input_tensor=None, input_shape=[120,120, 10],  pooling=None)
+    model.trainable = False
+  else:
+    model = imported_model(include_top=False, weights=None, input_tensor=None, input_shape=[120,120, 10],  pooling=None)
+    model.trainable = True
+  # add new classifier layers
+  flat = tf.keras.layers.Flatten()(model.layers[-1].output)
+  h1 = tf.keras.layers.Dense(1024, activation='elu')(flat)
+  h1 = tf.keras.layers.Dropout(0.25)(h1)
+  h2 = tf.keras.layers.Dense(512, activation='elu')(h1)
+  h2 = tf.keras.layers.Dropout(0.25)(h2)
+  clf = tf.keras.layers.Dense(256, activation='elu')(h2)
+  output = tf.keras.layers.Dense(1, activation='sigmoid',
+                                 bias_initializer=output_bias)(clf)
+  # define new model
+  model = tf.keras.models.Model(inputs=model.inputs, outputs=output)
+
+  model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
+              optimizer='adam',
+              metrics=metrics)
+  return model
+
+
+class TimeHistory(tf.keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.times = []
+
+    def on_epoch_begin(self, batch, logs={}):
+        self.epoch_time_start = time.time()
+
+    def on_epoch_end(self, batch, logs={}):
+        self.times.append(time.time() - self.epoch_time_start)
+
+
+def run_model(name, BATCH_SIZE=32, epochs=50, weights=False, architecture=ResNet50, pretrain=False):
+    print(50 * "*")
+    print(f"Running model: {name}")
+    print(50 * "=")
+    print(f"Batch Size: {BATCH_SIZE}")
+    if weights:
+        neg = 38400 - 984
+        pos = 984
+        total = neg + pos
+        neg_weight = (1 / neg) * (total) / 2.0
+        pos_weight = (1 / pos) * (total) / 2.0
+        class_weight = {0: neg_weight,
+                        1: pos_weight}
+        print(f"Using Class Weights: ")
+        print('\tWeight for Negative Class: {:.2f}'.format(neg_weight))
+        print('\tWeight for Positive Class: {:.2f}'.format(pos_weight))
+    else:
+        class_weight = None
+        print("Not Using Weights")
+
+    training_filenames = f'{tfr_path}/balanced_train_0.tfrecord'
+    validation_filenames = f'{tfr_path}/val-part-0.tfrecord'
+
+    training_data = get_training_dataset(training_filenames, batch_size=BATCH_SIZE)
+    val_data = get_validation_dataset(validation_filenames, batch_size=BATCH_SIZE)
+
+    len_val_records = 8768
+    len_train_records = 42240
+    steps_per_epoch = len_train_records // BATCH_SIZE
+    validation_steps = len_val_records // BATCH_SIZE
+
+    # Use an early stopping callback and our timing callback
+    early_stop = tf.keras.callbacks.EarlyStopping(
+        monitor='val_precision',
+        verbose=1,
+        patience=15,
+        mode='max',
+        restore_best_weights=True)
+
+    time_callback = TimeHistory()
+
+    model = build_model(imported_model=architecture,
+                        use_pretrain=pretrain)
+    model.summary()
+
+    history = model.fit(training_data,
+                        epochs=epochs,
+                        steps_per_epoch=steps_per_epoch,
+                        validation_data=val_data,
+                        validation_steps=validation_steps,
+                        callbacks=[time_callback, early_stop],
+                        class_weight=class_weight)
+    times = time_callback.times
+
+    model.save(f'{output_path}/{name}.h5')
+    df = pd.DataFrame(history.history)
+    df['times'] = time_callback.times
+    df.to_pickle(f'{output_path}/{name}.pkl')
+
+    return df
+
+if __name__ == 'main':
+    parser = argparse.ArgumentParser(description='Script for running different supervised classifiers')
+    parser.add_argument('-a', '--arch', options=[ResNet50, ResNet101V2, Xception, InceptionV3],
+                        help='Class of Model Architecture to use for classification')
+    parser.add_argument('-o', '--output', type=str,
+                        help='Output File Prefix for model file and dataframe')
+    parser.add_argument('-b', '--BATCH_SIZE', default=32, type=int,
+                       help="batch size to use during training and validation")
+    parser.add_argument('-e', '--EPOCHS', default=50, type=int,
+                        help="number of epochs to run")
+    parser.add_argument('-w', '--weights', default=False, type=bool,
+                       help="whether to use weights")
+    args = parser.parse_args()
+
+    if args.weights:
+        run_model(args.output,
+                  BATCH_SIZE=args.BATCH_SIZE,
+                  epochs=args.EPOCHS,
+                  weights=True,
+                  architecture=args.arch,
+                  pretrain=False)
+    else:
+        run_model(args.output,
+                  BATCH_SIZE=args.BATCH_SIZE,
+                  epochs=args.EPOCHS,
+                  weights=False,
+                  architecture=args.arch,
+                  pretrain=False)
+
---
+++ utils.py
@@ -0,0 +1,117 @@
+def read_tfrecord(example):
+    BAND_STATS = {
+        'mean': {
+            'B01': 340.76769064,
+            'B02': 429.9430203,
+            'B03': 614.21682446,
+            'B04': 590.23569706,
+            'B05': 950.68368468,
+            'B06': 1792.46290469,
+            'B07': 2075.46795189,
+            'B08': 2218.94553375,
+            'B8A': 2266.46036911,
+            'B09': 2246.0605464,
+            'B11': 1594.42694882,
+            'B12': 1009.32729131
+        },
+        'std': {
+            'B01': 554.81258967,
+            'B02': 572.41639287,
+            'B03': 582.87945694,
+            'B04': 675.88746967,
+            'B05': 729.89827633,
+            'B06': 1096.01480586,
+            'B07': 1273.45393088,
+            'B08': 1365.45589904,
+            'B8A': 1356.13789355,
+            'B09': 1302.3292881,
+            'B11': 1079.19066363,
+            'B12': 818.86747235
+        }
+    }
+
+    # Use this one-liner to standardize each feature prior to reshaping.
+    def standardize_feature(data, band_name):
+        return ((tf.dtypes.cast(data, tf.float32) - BAND_STATS['mean'][band_name]) / BAND_STATS['std'][band_name])
+
+    # decode the TFRecord
+    # The parse single example methods takes an example (from a tfrecords file),
+    # and a dictionary that explains the data format of each feature.
+    example = tf.io.parse_single_example(example, {
+        'B01': tf.io.FixedLenFeature([20 * 20], tf.int64),
+        'B02': tf.io.FixedLenFeature([120 * 120], tf.int64),
+        'B03': tf.io.FixedLenFeature([120 * 120], tf.int64),
+        'B04': tf.io.FixedLenFeature([120 * 120], tf.int64),
+        'B05': tf.io.FixedLenFeature([60 * 60], tf.int64),
+        'B06': tf.io.FixedLenFeature([60 * 60], tf.int64),
+        'B07': tf.io.FixedLenFeature([60 * 60], tf.int64),
+        'B08': tf.io.FixedLenFeature([120 * 120], tf.int64),
+        'B8A': tf.io.FixedLenFeature([60 * 60], tf.int64),
+        'B09': tf.io.FixedLenFeature([20 * 20], tf.int64),
+        'B11': tf.io.FixedLenFeature([60 * 60], tf.int64),
+        'B12': tf.io.FixedLenFeature([60 * 60], tf.int64),
+        'patch_name': tf.io.VarLenFeature(dtype=tf.string),
+        'original_labels': tf.io.VarLenFeature(dtype=tf.string),
+        'original_labels_multi_hot': tf.io.FixedLenFeature([43], tf.int64)
+    })
+
+    example['binary_label'] = example['original_labels_multi_hot'][tf.constant(12)]
+
+    # After parsing our data into a tensor, let's standardize and reshape.
+    reshaped_example = {
+        'B01': tf.reshape(standardize_feature(example['B01'], 'B01'), [20, 20]),
+        'B02': tf.reshape(standardize_feature(example['B02'], 'B02'), [120, 120]),
+        'B03': tf.reshape(standardize_feature(example['B03'], 'B03'), [120, 120]),
+        'B04': tf.reshape(standardize_feature(example['B04'], 'B04'), [120, 120]),
+        'B05': tf.reshape(standardize_feature(example['B05'], 'B05'), [60, 60]),
+        'B06': tf.reshape(standardize_feature(example['B06'], 'B06'), [60, 60]),
+        'B07': tf.reshape(standardize_feature(example['B07'], 'B07'), [60, 60]),
+        'B08': tf.reshape(standardize_feature(example['B08'], 'B08'), [120, 120]),
+        'B8A': tf.reshape(standardize_feature(example['B8A'], 'B8A'), [60, 60]),
+        'B09': tf.reshape(standardize_feature(example['B09'], 'B09'), [20, 20]),
+        'B11': tf.reshape(standardize_feature(example['B11'], 'B11'), [60, 60]),
+        'B12': tf.reshape(standardize_feature(example['B12'], 'B12'), [60, 60]),
+        'patch_name': example['patch_name'],
+        'original_labels': example['original_labels'],
+        'original_labels_multi_hot': example['original_labels_multi_hot'],
+        'binary_labels': example['binary_label']
+    }
+
+    # Next sort the layers by resolution
+    bands_10m = tf.stack([reshaped_example['B04'],
+                          reshaped_example['B03'],
+                          reshaped_example['B02'],
+                          reshaped_example['B08']], axis=2)
+
+    bands_20m = tf.stack([reshaped_example['B05'],
+                          reshaped_example['B06'],
+                          reshaped_example['B07'],
+                          reshaped_example['B8A'],
+                          reshaped_example['B11'],
+                          reshaped_example['B12']], axis=2)
+
+    # Finally resize the 20m data and stack the bands together.
+    img = tf.concat([bands_10m, tf.image.resize(bands_20m, [120, 120], method='bicubic')], axis=2)
+
+    multi_hot_label = reshaped_example['original_labels_multi_hot']
+    binary_label = reshaped_example['binary_labels']
+
+    # Can update this to return the multilabel if doing multi-class classification
+    return img, binary_label
+
+
+def get_batched_dataset(filenames, batch_size):
+    option_no_order = tf.data.Options()
+    option_no_order.experimental_deterministic = False
+
+    dataset = tf.data.Dataset.list_files(filenames, shuffle=True)
+    print(f'Filenames: {filenames}')
+    dataset = dataset.with_options(option_no_order)
+    dataset = dataset.interleave(tf.data.TFRecordDataset, cycle_length=2, num_parallel_calls=1)
+    dataset = dataset.shuffle(buffer_size=2048).repeat()
+
+    dataset = dataset.map(read_tfrecord, num_parallel_calls=10)
+    dataset = dataset.batch(batch_size, drop_remainder=False)  # drop_remainder will be needed on TPU
+    dataset = dataset.prefetch(5)  #
+
+    return dataset
