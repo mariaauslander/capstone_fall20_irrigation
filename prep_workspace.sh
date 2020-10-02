55e566cabecc009b3d41fbc739ed5f773507e384
tom <tomgoter@gmail.com>
2020-10-02 15:18:07 -0400

    updated to work together

--- prep_workspace.sh
+++ prep_workspace.sh
@@ -2,26 +2,3 @@ mkdir BigEarthData
 mkdir BigEarthData/models
 mkdir BigEarthData/tfrecords
 ls -l BigEarthData
-
-apt install python3-pip
-
-pip3 install python
-pip3 install --upgrade pip
-
-apt update
-apt install software-properties-common
-add-apt-repository ppa:deadsnakes/ppa
-apt update
-apt install vim
-apt install python3.7
-
-alias python='python3.7'
-
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
-pip install pandas
-pip install tensorflow
-pip install rasterio
-pip install matplotlib
-pip install tqdm
-pip install seaborn
