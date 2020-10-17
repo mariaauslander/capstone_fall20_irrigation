31d07934dc1e97b287b1ba66c18a2832ce222e46
tom <tomgoter@gmail.com>
2020-10-17 11:28:03 -0400

    removed gaussian blur from helpers

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from augmentation.gaussian_filter import GaussianBlur
+#from augmentation.gaussian_filter import GaussianBlur
 
 
 def get_negative_mask(batch_size):
@@ -14,9 +14,9 @@ def get_negative_mask(batch_size):
     return tf.constant(negative_mask)
 
 
-def gaussian_filter(v1, v2):
-    k_size = int(v1.shape[1] * 0.1)  # kernel size is set to be 10% of the image height/width
-    gaussian_ope = GaussianBlur(kernel_size=k_size, min=0.1, max=2.0)
-    [v1, ] = tf.py_function(gaussian_ope, [v1], [tf.float32])
-    [v2, ] = tf.py_function(gaussian_ope, [v2], [tf.float32])
-    return v1, v2
+# def gaussian_filter(v1, v2):
+#     k_size = int(v1.shape[1] * 0.1)  # kernel size is set to be 10% of the image height/width
+#     gaussian_ope = GaussianBlur(kernel_size=k_size, min=0.1, max=2.0)
+#     [v1, ] = tf.py_function(gaussian_ope, [v1], [tf.float32])
+#     [v2, ] = tf.py_function(gaussian_ope, [v2], [tf.float32])
+#     return v1, v2
