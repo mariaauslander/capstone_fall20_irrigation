937c5f8ec7bf9545de5cc9d2ed83670ab660aabe
tom <tomgoter@gmail.com>
2020-10-26 18:51:23 -0400

    change to store ft results

--- run_ft.sh
+++ run_ft.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 for epoch in 2 4 6 8 10 12
-do
-for perc in 1 3 13
-do
+ do
+ for perc in 1 3 13
+  do
    python finetune.py -p simclr_100_t3_s50_${epoch}.h5 \
                       -t balanced_train_${perc}percent.tfrecord \
                       -e 250 \
-                      -o simclr_ft_100_do50_lr0001_${perc}p_${epoch}e
+                      -o simclr_ft_100_do50_lr0001_${perc}p_${epoch}e >> ft_${epoch}_epoch.out
 # End over percentages
 done
+cp ft_${epoch}_epoch.out /data
 # End over epochs
 done
 #
