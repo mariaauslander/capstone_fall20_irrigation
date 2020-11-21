e3aade898fa355445f8ed10638bd779231debbb9
tom <tomgoter@gmail.com>
2020-11-21 11:01:23 -0500

    changed bash script for ft studies

--- run_ft.sh
+++ run_ft.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for epoch in 10
+for epoch in 20 30 35 40 45
  do
- for perc in 1 3 13
+ for perc in 1 3 10
   do
-   python finetune.py -p simclr_100_t3_s50_${epoch}.h5 \
+   python finetune.py -p ca_simclr_rn101_s50_t1_50_${epoch}.h5 \
                       -t final_balanced_train_${perc}percent.tfrecord \
                       -e 250 \
-                      -o FINAL_simclr_ft_100_do50_lr00005_${perc}p_${epoch}e >> ft_${epoch}_epoch.out
+                      -o ca_simclr_rn101_t1_e${epoch}_mlp_${perc}perc >> ft_${epoch}_epoch.out
 # End over percentages
 done
 cp ft_${epoch}_epoch.out /data
