162f85dedbaebf9fa4344815fd5b13fe9cac9f45
tom <tomgoter@gmail.com>
2020-11-22 12:02:07 -0500

    changed bash script for ft studies

--- run_ft.sh
+++ run_ft.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-for epoch in 20 30 35 40 45
+for epoch in 5 10 15 20 25 30 35 40 45 50
  do
  for perc in 1 3 10
   do
    python finetune.py -p ca_simclr_rn101_s50_t1_50_${epoch}.h5 \
                       -t final_balanced_train_${perc}percent.tfrecord \
                       -e 250 \
-                      -o ca_simclr_rn101_t1_e${epoch}_mlp_${perc}perc >> ft_${epoch}_epoch.out
+                      -o ca_simclr_rn101_t1_e${epoch}_1ph_${perc}perc \
+                      -u 1 >> ft_1ph_${epoch}_epoch.out
 # End over percentages
 done
 cp ft_${epoch}_epoch.out /data
@@ -15,9 +16,3 @@ rsync -aP ./BigEarthData/models /data
 # End over epochs
 done
 #
-#python finetune.py -p simclr_100_t3_s50_${epoch}.h5 \
-#                      -t balanced_train_*.tfrecord \
-#                      -e 250 \
-#                      -o simclr_ft_100_do50_lr00005_100p_${epoch}e >> ft_${epoch}_epoch.out
-#cp ft_${epoch}_epoch.out /data
-#rsync -aP ./BigEarthData/models /data
